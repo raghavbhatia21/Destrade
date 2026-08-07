837207886e1dcb400e0f3e523fb474de3d545d14
RaghavBhatia <raghavbhatia21@gmail.com>
2026-08-07 22:07:47 +0530

    RemoveObsoleteTestFilesAndScratchDirectory

--- scratch/check_lines.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('d:/Code of Content/destrade-mobile/www/js/nse-api.js', 'utf8');
-const lines = content.split('\n');
-console.log('Total Lines:', lines.length);
-for (let i = 810; i <= 830; i++) {
-    if (lines[i - 1] !== undefined) {
-        console.log(`Line ${i}: ${JSON.stringify(lines[i - 1])}`);
-    }
-}
--- scratch/copy_app.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.copyFileSync('d:\\Code of Content\\destrade\\js\\app.js', 'd:\\Code of Content\\destrade-mobile\\www\\js\\app.js');
-console.log('✅ Copied clean destrade/js/app.js to destrade-mobile/www/js/app.js');
-
-fs.copyFileSync('d:\\Code of Content\\destrade\\js\\nse-api.js', 'd:\\Code of Content\\destrade-mobile\\www\\js\\nse-api.js');
-console.log('✅ Copied clean destrade/js/nse-api.js to destrade-mobile/www/js/nse-api.js');
